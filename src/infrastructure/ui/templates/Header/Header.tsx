07318aa44c64324b4b15df50190d79c5166d542c
Ulises Santana <ulisesantana@gmail.com>
2024-09-02 22:22:36 +0100

    💄 improve styles for Header

--- src/infrastructure/ui/templates/Header/Header.tsx
+++ src/infrastructure/ui/templates/Header/Header.tsx
@@ -3,14 +3,14 @@ import {LogoutButton} from "infrastructure/ui/atoms";
 import './Header.css'
 
 export interface HeaderProps {
-    onLogout: MouseEventHandler
+  onLogout: MouseEventHandler
 }
 
 export function Header({onLogout}: HeaderProps) {
-    return <header>
-        <i>Misperrapp</i>
-        <div className="clock">
-            <LogoutButton onLogout={onLogout}/>
-        </div>
-    </header>
+  return <header>
+    <span>
+      <i>Misperrapp</i>
+    </span>
+    <LogoutButton onLogout={onLogout}/>
+  </header>
 }
