9bfbf3a78c858df1a92bfd7472464de5ceafcc5d
Ulises Santana <ulisesantana@gmail.com>
2024-10-12 00:08:19 +0100

    💄 improve transaction card styles

--- src/infrastructure/ui/components/TransactionCard/TransactionCard.tsx
+++ src/infrastructure/ui/components/TransactionCard/TransactionCard.tsx
@@ -1,20 +1,31 @@
 import { Transaction } from "@domain/models";
 import "./TransactionCard.css";
+import { generatePath, routes } from "@infrastructure/ui/routes";
+import { Link } from "wouter";
 
 export function TransactionCard({ transaction }: { transaction: Transaction }) {
   const date = `${transaction.date.getFormatedDate()} / ${transaction.date.getFormatedMonth()}`;
+  const detailsPath = generatePath(routes.transactions.details, {
+    id: transaction.id.toString(),
+  });
   return (
-    <div className="transaction-card">
-      <div className="transaction-card-date">{date}</div>
-      <div className="transaction-card-body">{transaction.category.title}</div>
-      <div
-        className={
-          "transaction-card-amount " +
-          (transaction.isOutcome() ? "outcome" : "income")
-        }
-      >
-        {transaction.amountFormatted}
-      </div>
+    <div className="transaction-card-container">
+      <Link to={detailsPath}>
+        <div className="transaction-card">
+          <div className="transaction-card-date">{date}</div>
+          <div className="transaction-card-body">
+            {transaction.category.title}
+          </div>
+          <div
+            className={
+              "transaction-card-amount " +
+              (transaction.isOutcome() ? "outcome" : "income")
+            }
+          >
+            {transaction.amountFormatted}
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
--- src/infrastructure/ui/components/TransactionList/TransactionList.tsx
+++ src/infrastructure/ui/components/TransactionList/TransactionList.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Transaction } from "@domain/models";
 import "./TransactionList.css";
-import { Link } from "wouter";
-import { generatePath, routes } from "@infrastructure/ui/routes";
 
 import { TransactionCard } from "../TransactionCard";
 
@@ -15,18 +13,12 @@ export function TransactionList({ transactions }: TransactionListProps) {
     <ul className="transaction-card-list">
       {React.Children.toArray(
         transactions.map((transaction) => (
-          <Link
-            to={generatePath(routes.transactions.details, {
-              id: transaction.id.toString(),
-            })}
+          <li
+            data-id={transaction.id.toString()}
+            key={transaction.id.toString()}
           >
-            <li
-              data-id={transaction.id.toString()}
-              key={transaction.id.toString()}
-            >
-              <TransactionCard transaction={transaction} />
-            </li>
-          </Link>
+            <TransactionCard transaction={transaction} />
+          </li>
         )),
       )}
     </ul>
