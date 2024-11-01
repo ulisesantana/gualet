8dd48c788269094dc4dfe8920ea75c291a3658b4
Ulises Santana <ulisesantana@gmail.com>
2024-11-01 11:57:17 +0000

    test: add test for save transaction use case

---
+++ src/application/cases/save-transaction/save-transaction.use-case.test.ts
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, it, Mocked, vi } from "vitest";
+import { SaveTransactionUseCase } from "@application/cases";
+import { TransactionRepository } from "@application/repositories";
+import { Transaction } from "@domain/models";
+import { TransactionBuilder } from "@test/builders";
+
+describe("SaveTransactionUseCase", () => {
+  let mockTransactionRepository: Mocked<TransactionRepository>;
+  let saveTransactionUseCase: SaveTransactionUseCase;
+
+  beforeEach(() => {
+    mockTransactionRepository = {
+      save: vi.fn(),
+    } as unknown as Mocked<TransactionRepository>;
+    saveTransactionUseCase = new SaveTransactionUseCase(
+      mockTransactionRepository,
+    );
+  });
+
+  it("should save a transaction", async () => {
+    const transaction: Transaction = new TransactionBuilder().build();
+
+    await saveTransactionUseCase.exec(transaction);
+
+    expect(mockTransactionRepository.save).toHaveBeenCalledWith(transaction);
+  });
+
+  it("should throw an error if repository fails to save transaction", async () => {
+    const transaction: Transaction = new TransactionBuilder().build();
+
+    // Simulate an error during save
+    mockTransactionRepository.save.mockRejectedValue(
+      new Error("Failed to save transaction"),
+    );
+
+    await expect(saveTransactionUseCase.exec(transaction)).rejects.toThrow(
+      "Failed to save transaction",
+    );
+  });
+});
