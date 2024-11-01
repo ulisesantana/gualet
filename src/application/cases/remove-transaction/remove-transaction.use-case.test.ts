a45d5000795a94a151d8fd1b909fac84254458bf
Ulises Santana <ulisesantana@gmail.com>
2024-11-01 11:45:10 +0000

    test: add test for remove transaction use case

---
+++ src/application/cases/remove-transaction/remove-transaction.use-case.test.ts
@@ -0,0 +1,29 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { TransactionRepository } from "@application/repositories";
+import { Id } from "@domain/models";
+
+import { RemoveTransactionUseCase } from "./remove-transaction.use-case";
+
+describe("RemoveTransactionUseCase", () => {
+  let mockTransactionRepository: TransactionRepository;
+  let removeTransactionUseCase: RemoveTransactionUseCase;
+
+  beforeEach(() => {
+    mockTransactionRepository = {
+      remove: vi.fn(),
+    } as unknown as TransactionRepository;
+    removeTransactionUseCase = new RemoveTransactionUseCase(
+      mockTransactionRepository as unknown as TransactionRepository,
+    );
+  });
+
+  it("should remove a transaction by the given id", async () => {
+    const transactionId: Id = new Id();
+
+    await removeTransactionUseCase.exec(transactionId);
+
+    expect(mockTransactionRepository.remove).toHaveBeenCalledWith(
+      transactionId,
+    );
+  });
+});
