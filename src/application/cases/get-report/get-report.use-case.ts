1f8e9e242a8c4bcd718220fa7858b98191a472f2
Ulises Santana <ulisesantana@gmail.com>
2024-11-09 15:53:45 +0000

    feat: add get report use case

--- src/application/cases/get-report/get-report.use-case.ts
+++ src/application/cases/get-report/get-report.use-case.ts
@@ -10,12 +10,9 @@ type Output = Promise<Report>;
 
 export class GetReportUseCase implements UseCase<Input, Output> {
   constructor(private readonly repository: TransactionRepository) {}
-  async exec(input: Input) {
-    let from = input.from;
-    let to = input.to;
-    if (from && to && to.earlierThan(from)) {
-      from = input.to;
-      to = input.from;
+  async exec({ from, to }: Input): Promise<Report> {
+    if (to.earlierThan(from)) {
+      return this.exec({ from: to, to: from });
     }
     const transactions = await this.repository.find({ from, to });
     return new Report({ from, to, transactions });
