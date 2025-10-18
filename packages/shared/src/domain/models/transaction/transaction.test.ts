f9e070356854de5d3d26d7b097120d63c88af254
Ulises Santana <ulisesantana@gmail.com>
2025-10-18 10:59:00 +0100

    fix(dependencies): update core dependency to shared and improve restart script

--- packages/shared/src/domain/models/transaction/transaction.test.ts
+++ packages/shared/src/domain/models/transaction/transaction.test.ts
@@ -1,5 +1,5 @@
 import {describe, expect, it} from "vitest";
-import {Category, Day, Id, OperationType, PaymentMethod, Transaction} from "@gualet/core";
+import {Category, Day, Id, OperationType, PaymentMethod, Transaction} from "@gualet/shared";
 
 
 describe("Transaction", () => {
